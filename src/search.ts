528654226beeabaf9e93358da58a528a70606d9e
Steven Jimenez <stevenmjimenez@gmail.com>
2021-04-07 23:45:46 -0400

    Added search-query

---
+++ src/search.ts
@@ -0,0 +1,164 @@
+import axios from "axios";
+
+// Not a good way to store this, but this isn't a production application.
+const API_KEY: string = 'ac4197870e456b0d4b6fddc5c77e8b3f';
+
+// Root URL for the API.
+const API_URL: string = 'http://ws.audioscrobbler.com/2.0/';
+
+export interface SearchParams {
+    /**
+     * The artist's name to search.
+     */
+    artist: string;
+
+    /**
+     * The number of results to fetch per page.
+     * @default 30
+     */
+    limit?: number;
+
+    /**
+     * The page number to fetch.
+     * @default 1
+     */
+    page?: number;
+}
+
+/**
+ * The parsed search-results from the server.
+ */
+export interface Results {
+    meta: Metadata;
+    artists: Artist[];
+}
+
+/**
+ * Provides information about an individual artist.
+ */
+export interface Artist {
+    images: Record<ImageSize, string | null>;
+    listeners: number;
+    mbid: string;
+    name: string;
+    streamable: boolean;
+    url: string;
+}
+
+/**
+ * Provides metadata about the response.
+ */
+export interface Metadata {
+    page: number;
+    pageLen: number;
+    pageCount: number;
+    numResults: number;
+}
+
+export type ImageSize = 'small' | 'medium' | 'large' | 'extralarge' | 'mega';
+
+/**
+ * Execute an anonymous search against the API
+ */
+export async function search(params: SearchParams): Promise<Results | null> {
+    const parameters: ApiParameters = {
+        method: 'artist.search',
+        api_key: API_KEY,
+        artist: params.artist,
+        format: 'json'
+    };
+
+    if (params.limit) {
+        parameters.limit = `${params.limit}`;
+    }
+
+    if (params.page) {
+        parameters.page = `${params.page}`;
+    }
+
+    const response = await axios.get(`${API_URL}?${joinToString(parameters)}`, {responseType: 'json'});
+
+    const results: unknown = response?.data?.results;
+    if (!results) {
+        return null;
+    }
+
+    // Normally I'd do a better job of type-checking this.
+    return parseResults(results as RawResults);
+}
+
+interface ApiParameters {
+    method: string;
+    api_key: string;
+
+    [key: string]: string;
+}
+
+function joinToString(parameters: ApiParameters): string {
+    return Object.keys(parameters).map(name => `${name}=${parameters[name]}`).join('&');
+}
+
+interface RawImage {
+    '#text': string;
+    size: ImageSize;
+}
+
+interface RawArtist {
+    image: RawImage[];
+    listeners: string;
+    mbid: string;
+    name: string;
+    streamable: string;
+    url: string;
+}
+
+interface RawResults {
+    artistmatches: {
+        artist: RawArtist[];
+    }
+    'opensearch:Query': {
+        '#text': string;
+        role: 'request';
+        searchTerms: string;
+        startPage: string;
+    }
+    'opensearch:itemsPerPage': string;
+    'opensearch:startIndex': string;
+    'opensearch:totalResults': string;
+}
+
+function parseResults(results: RawResults): Results {
+    return {
+        artists: results.artistmatches.artist.map(parseArtist),
+        meta: parseMeta(results)
+    }
+}
+
+function parseArtist(raw: RawArtist): Artist {
+    return {
+        mbid: raw.mbid,
+        name: raw.name,
+        url: raw.url,
+        listeners: Number(raw.listeners),
+        streamable: raw.streamable === '1',
+        images: raw.image.reduce<Artist['images']>((acc, val) => {
+            acc[val.size] = val["#text"];
+            return acc;
+        }, {
+            small: null,
+            medium: null,
+            large: null,
+            extralarge: null,
+            mega: null
+        })
+    };
+}
+
+function parseMeta(raw: RawResults): Results['meta'] {
+    const numResults = Number(raw['opensearch:totalResults']);
+    const page = Number(raw['opensearch:Query'].startPage);
+    const pageLen = Number(raw['opensearch:itemsPerPage']);
+    const pageCount = Math.ceil(numResults / pageLen);
+
+    return {numResults, page, pageLen, pageCount};
+}
